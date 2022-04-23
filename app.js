3debefdb7af949aba3cae897ce2138c971679e84
xkj <1767822853@qq.com>
2022-04-24 01:30:16 +0800

    Update app.js
    只有失败的时候才发送消息

--- app.js
+++ app.js
@@ -68,10 +68,18 @@ async function start() {
     let t2 = content.match(/【签到奖励】:((.|\n)*)【其他奖励】/)
     let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
 
-    console.log(res);
-      console.log(res2);
     
-    await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+    console.log("结果:" + res);
+
+    if(res === "失败"){
+          await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+    }
+        
+    
+      
+      
+    
+    
   }
 }
 
